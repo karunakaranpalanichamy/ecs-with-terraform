5522441d0b0416b453b7b332c67b4f3a37abd3f8
kpalanichamy <karunakaran.palanichamy@changehealthcare.com>
2023-05-17 08:19:16 -0400

    updated the account name

--- provider.tf
+++ provider.tf
@@ -1,18 +1,3 @@
-terraform {
-  cloud {
-    organization = "harishankard29"
-    workspaces {
-      name = "demo-ecs"
-    }
-  }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.aws_region
-  # profile = var.aws_profile
+    region= "us-east-2"
 }
